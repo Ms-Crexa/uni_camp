c44ad1bd2983b4e73be967cf6b9eb69f10e818f1
pacasiano <peterandre.casiano@icloud.com>
2024-11-17 22:07:48 +0800

    feat: Error 401 redesign

--- lib/src/screens/unauthorized_page.dart
+++ lib/src/screens/unauthorized_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:uni_camp/src/screens/signin_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class UnauthorizedPage extends StatefulWidget {
   const UnauthorizedPage({super.key});
@@ -16,29 +17,88 @@ class _UnauthorizedPageState extends State<UnauthorizedPage> {
     FirebaseAuth.instance.signOut();
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Access Denied"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "You do not have access to this page.",
-              style: TextStyle(fontSize: 18),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/bg.png'),
+                fit: BoxFit.cover,
+              ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignInPage()),
-                );
-              },
-              child: const Text("Go to Sign In"),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.5),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "ERROR 401: Unauthorized Access",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: GoogleFonts.jost().fontFamily,
+                  ),
+                ),
+                const Text(
+                  "You Do Not Have Access To This Page.",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Color.fromARGB(255, 185, 185, 185),
+                    fontWeight: FontWeight.normal,
+                    // fontFamily: GoogleFonts.jost().fontFamily,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 200,
+                  height: 30,
+                  child: Stack(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignInPage()),
+                          );
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(
+                              const Color.fromARGB(255, 255, 255, 255)),
+                          shape: WidgetStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                          fixedSize: WidgetStateProperty.all(
+                            const Size(200, 30),
+                          ),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Return to sign-in page',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 22, 22, 22),
+                                fontWeight: FontWeight.w600,
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
